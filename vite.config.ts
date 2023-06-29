a930ac10f9813ab24f0557a39705393af98f40d5
Jackson Coleman <wcoleman@colgate.edu>
2023-06-29 17:21:13 -0500

    Removed excess nonsense

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'docs',
-    rollupOptions: {
-      input: '/src/main.tsx',
-    },
-  },
 });
