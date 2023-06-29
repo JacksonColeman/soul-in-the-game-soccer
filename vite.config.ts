97f920348558445cb0f08e6bc6222c601b10ec93
Jackson Coleman <wcoleman@colgate.edu>
2023-06-29 17:07:37 -0500

    trying again

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/soul-in-the-game-soccer',
   build: {
     outDir: 'docs',
+    rollupOptions: {
+      input: '/src/main.tsx',
+    },
   },
 });
